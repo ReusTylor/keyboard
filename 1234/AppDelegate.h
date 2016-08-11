f518e5d88c577a5a5254684d5aba283e02ea827f
创新创业中心 <cxcy@cxcy-imac.lan>
2016-08-11 09:55:20 +0800

    Initial Commit

---
+++ 1234/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  1234
+//
+//  Created by 创新创业中心 on 16/8/11.
+//  Copyright © 2016年 UESTCACM QKTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 1234/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  1234
+//
+//  Created by 创新创业中心 on 16/8/11.
+//  Copyright © 2016年 UESTCACM QKTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
